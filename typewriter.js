6e8091e5d58d62bba9873730d0d5a2472be5eec8
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-04-20 15:52:19 -0400

    typing a line in command line as a typewriter

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 50;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char); // print the char here
+  }, delay);
+  delay += 50;
+}
+setTimeout(() => {
+  console.log();
+}, delay + 50);
